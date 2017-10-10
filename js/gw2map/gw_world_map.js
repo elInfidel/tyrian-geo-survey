48b65961aa490703ae1ef43868395563d29d90e6
Liam Parker <parker.liam5@gmail.com>
2017-10-10 23:51:29 +1100

    Formatted File

--- js/gw2map/gw_world_map.js
+++ js/gw2map/gw_world_map.js
@@ -18,79 +18,69 @@ initializeMap();
 downloadWorldData();
 
 // Initialize Leaflet API and create a map based on the div id 'map'
-function initializeMap()
-{
-	map = L.map("map", {
-		minZoom: 2,
-		maxZoom: 7,
-		reuseTiles: true,
-    crs: L.CRS.Simple
-	});
+function initializeMap() {
+    map = L.map("map", {
+        minZoom: 2,
+        maxZoom: 7,
+        reuseTiles: true,
+        crs: L.CRS.Simple
+    });
 }
 
 // Downloads informations about the continents and maps in the game world.
 // Stores them in variables for access elsewhere.
-function downloadWorldData()
-{
-	// Load continent JSON async.
-	jQuery.getJSON("https://api.guildwars2.com/v2/continents?ids=all", function(data)
-	{
-			// Load first available continent
-	    continentList = data;
-			renderContinent(continentList[0]);
-	});
+function downloadWorldData() {
+    // Load continent JSON async.
+    jQuery.getJSON("https://api.guildwars2.com/v2/continents?ids=all", function(data) {
+        // Load first available continent
+        continentList = data;
+        renderContinent(continentList[0]);
+    });
 
-	// Load map data async
-	jQuery.getJSON("https://api.guildwars2.com/v1/maps", function(data)
-	{
-			mapData = new Object();
+    // Load map data async
+    jQuery.getJSON("https://api.guildwars2.com/v1/maps", function(data) {
+        mapData = new Object();
 
-			for (var object in data.maps)
-			{
-				// We need to check if we're dealing with a map or some other undefined junk within the maps object
-				// Typical javascript rubbish.
-    		if (!data.maps.hasOwnProperty(object)) continue;
-    		var m =  data.maps[object];
+        for (var object in data.maps) {
+            // We need to check if we're dealing with a map or some other undefined junk within the maps object
+            // Typical javascript rubbish.
+            if (!data.maps.hasOwnProperty(object)) continue;
+            var m = data.maps[object];
 
-				if(mapData[m.continent_id] == undefined)
-					mapData[m.continent_id] = []
+            if (mapData[m.continent_id] == undefined)
+                mapData[m.continent_id] = []
 
-				mapData[m.continent_id].push(m)
-			}
+            mapData[m.continent_id].push(m)
+        }
 
-			populateContinent(continentList[0]);
-	});
+        populateContinent(continentList[0]);
+    });
 }
 
 // Displays a given continent with leaflet
-function renderContinent(continent)
-{
-	// set leaflet map scroll bounds
-	var dimWidth = continent.continent_dims[0];
-	var dimHeight = continent.continent_dims[1];
-	map.setMaxBounds(L.latLngBounds(unproject([0, 0]), unproject([dimWidth, dimHeight])));
+function renderContinent(continent) {
+    // set leaflet map scroll bounds
+    var dimWidth = continent.continent_dims[0];
+    var dimHeight = continent.continent_dims[1];
+    map.setMaxBounds(L.latLngBounds(unproject([0, 0]), unproject([dimWidth, dimHeight])));
 
-	// Load tiles
-	L.tileLayer("https://tiles{s}.guildwars2.com/" + continent.id + "/1/{z}/{x}/{y}.jpg",
-	{
-		continuousWorld: true,
-		subdomains: '1234' // current tile service subdomains
-	}).addTo(map);
+    // Load tiles
+    L.tileLayer("https://tiles{s}.guildwars2.com/" + continent.id + "/1/{z}/{x}/{y}.jpg", {
+        continuousWorld: true,
+        subdomains: '1234' // current tile service subdomains
+    }).addTo(map);
 
-	map.fitWorld();
+    map.fitWorld();
 }
 
 // Populates a continents contained regions
-function populateContinent(continent)
-{
-	mapData[continent.id].forEach(function(m)
-	{
-		
-	});
+function populateContinent(continent) {
+    mapData[continent.id].forEach(function(m) {
+
+    });
 }
 
 // Unprojects the supplied coordinates onto the map
-function unproject(coord)
-{
+function unproject(coord) {
     return map.unproject(coord, map.getMaxZoom());
 }
